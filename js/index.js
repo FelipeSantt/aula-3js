b903d1d1e016cf4d420900e442c49a5d970ded28
Felipe Dos Santos Sampaio <felipe0523@digitalcollegesul.local>
2023-12-16 17:00:19 -0300

    subindo aplicação

--- js/index.js
+++ js/index.js
@@ -1,7 +1,33 @@
-let numeroSorteado = Number((Math.random() * 11).toFixed());
-let alunos = ['Afonso','Marcio','Nathan','Caio','Isabela','Felipe','Santos','Rodrigo','Joao','Junior','Gabriel','Nicolas'];
-alunoSorteado.innerHTML = `${numeroSorteado + 1} - ${alunos[numeroSorteado]}`;
 
+let nomeMes = ['Rei','Mo','Cupido','Apaixonado','pagodeiro','Cara Perfeito','Mensageiro','Gueri Gueri','Coração','Linda Voz','Criado mudo'];
+
+
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+    let nomeDia = '';
+    if(Number(dia.value) < 4){
+        nomeDia = 'que tanto fez ou tanto faz'
+    }else if (Number(dia.value < 7)) {
+        nomeDia = 'que desabou o barraco';
+    }
+    resultado.innerHTML = `Seu nome de pagodeiro é: ${nomeMes[mes.value]} ${nomeDia}`;
+}
+
+
+
+
+
+
+
+//Variavel declarada transfomando string para Number e usando o metodo Math.random para pegar um numero aleatorio e multiplica pela quantidade de numero que voce deseja no sorteio, e por ultimo o to.Fixed para declarar o tamanho da casa decimal nesse caso pegando so o numero inteiro.
+// let numeroSorteado = Number((Math.random() * 11).toFixed());
+
+//Maneira de Fazer com uma Array
+// let alunos = ['Afonso','Marcio','Nathan','Caio','Isabela','Felipe','Santos','Rodrigo','Joao','Junior','Gabriel','Nicolas'];
+// alunoSorteado.innerHTML = `${numeroSorteado + 1} - ${alunos[numeroSorteado]}`;
+
+//Maneira de Fazer com Switch
 // switch(numeroSorteado){
 //     case 1:
 //         alunoSorteado.innerHTML = `${numeroSorteado} - Afonso`;
